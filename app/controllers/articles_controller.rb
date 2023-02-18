7a4f11e8c9bf542b7511be7f17c68975fc0e3fe9
mostafatarek <mostaf01230@gmail.com>
2023-02-18 16:27:49 +0200

    Add show action and update create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,19 @@ class ArticlesController < ApplicationController
         @article=Article.new
     end
     def create
-        render plain: params[:article].inspect
+        # render plain: params[:article].inspect
         @article=Article.new(article_params)
-        @article.save
-        redirect_to article_show(@article)
+        if @article.save
+            flash[:notice]="Article was succesfully uploaded"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+
+       
+    end
+    def show 
+        @article=Article.find(params[:id])
     end
     private 
     def article_params
